d5d58414f14ddcfa27f1f9701357c09873b756e5
Timofey Kuzmin <sinnerpocht@gmail.com>
2019-05-14 23:39:17 +0300

    download jpg files from site page by extension only from setting site url

--- classmodule.py
+++ classmodule.py
@@ -1,11 +1,14 @@
 import argparse
 import requests
+from lxml import etree, html
+import re, os.path as path
 
 
 class FilesScrapper:
-    """
-
-    """
+    """"
+    Utilit for downloading files from page by allowed extension
+    Save all files in directories with the same names
+    """""
 
     def __init__(self):
         parser = argparse.ArgumentParser(description="Recursive copy static files by extension from site")
@@ -18,6 +21,13 @@ class FilesScrapper:
         args = parser.parse_args()
 
         self.main_link = args.link
+
+        self.extension = args.allow
+
+        self.files_limit = int(args.limit)
+
+        self.reg = re.compile('%s.*.(%s)$' % (self.main_link, re.escape(self.extension)))
+        # print(self.reg)
 
         if not args.link:
             print('Error! Main link not defined!')
@@ -35,41 +45,73 @@ class FilesScrapper:
                         }
 
         self.files_list = self.get_files_links(self.main_link)
-
-        print(self.files_list)
-
+        # print(self.files_list)
         self.parse_files()
 
-    def parse_files(self):
-        # i = 1
-        # while True:
-        #     print('Итерация № %d...' % i)
-        #     new_files = self.files_list - self.finished_files
-        #
-        #     if new_files and len(new_files):
-        #         self.get_files(new_files)
-        #     else:
-        #         break
-
-        for file in self.files_list:
-            print(file)
-
-    """
-    Get files links from current page
-    """
-    def get_files_links(self, page):
-
+    def get_page(self, page):
         try:
             r = self.session.get(page, headers=self.headers)
         except requests.exceptions.RequestException as e:
             print('Something wrong! Was error while parsing link {}\n'.format(page))
             print(e)
+            return None
         else:
-            if r.status_code == 200:
-                print(111)
-        return set()
+            return r
 
+    def parse_files(self):
 
-    def get_files(self, files_list):
-        file_link = str()
-        self.finished_files.add(file_link)
+        for file in self.files_list:
+            if len(self.finished_files) < self.files_limit:
+                attr, link = file[1], file[2].strip()
+                print(attr, link)
+                # print(re.match(r'\.(jpg|png|jpeg|svg)$', link.strip()))
+                if attr is not None and re.search(self.reg, link):
+                    self.get_file(link)
+            else:
+                return
+
+            # print(file[2])
+
+    """
+    Get files links from current page
+    """
+
+    def get_files_links(self, page):
+
+        response = self.get_page(page)
+
+        if not response:
+            return set()
+
+        if response.status_code != 200:
+            print('Ошибка обращения по ссылке {}. Страница вернула {} код'.format(page, response.status_code))
+        else:
+            try:
+                parsed_body = html.fromstring(response.content)
+                """
+                Отлавливем исключение, если документ не содержит html 
+                 """
+            except etree.ParserError as e:
+                print(e)
+                return set()
+            else:
+                # Получаем все ссылки со страницы
+                parsed_body.make_links_absolute(self.main_link)
+                links = parsed_body.iterlinks()
+                return set(links)
+
+    def get_file(self, file_link):
+
+        # If we downloaded this file early, then quit
+        if file_link in self.finished_files:
+            return None
+
+        print('Downloading {}...'.format(file_link))
+        filename = path.basename(file_link)
+        response = requests.get(file_link)
+
+        if response.status_code == 200:
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            # Save file_link in memory
+            self.finished_files.add(file_link)
